47f33064e9a9fb8e3c05dcbb6963cc45502c1d73
Marina.S. <marina.sevostian@gmail.com>
2025-01-20 02:22:49 +0200

    add search bar and toaster

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
-// import { useEffect, useState } from 'react';
-// import fetchImages from './services/api';
+import { useEffect, useState } from 'react';
+import SearchBar from './components/SearchBar/SearchBar';
+import fetchImages from './services/api';
+import ImageGallery from './components/ImageGallery/ImageGallery';
+import { Toaster } from 'react-hot-toast';
 import './App.css';
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [isError, setIsError] = useState(false);
+  const [image, setImage] = useState([]);
 
-  return <></>;
+  useEffect(() => {
+    if (!query) return;
+    const getData = async () => {
+      try {
+        setIsError(false);
+        const data = await fetchImages(query, page);
+        setImage(prev => [...prev, ...data.results]);
+      } catch {
+        setIsError(true);
+      } finally {
+        console.log(getData);
+      }
+    };
+    getData();
+  }, [query, page]);
+
+  const handleSetQuery = searchValue => {
+    setQuery(searchValue);
+    setImage([]);
+    setPage(1);
+  };
+
+  return (
+    <>
+      <SearchBar setQuery={handleSetQuery} />
+      <Toaster />
+      {isError}
+      <ImageGallery images={image} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,6 @@
+// import ImageCard from '../ImageCard/ImageCard';
+// import s from './ImageGallery.module.css';
+
+const ImageGallery = () => {};
+
+export default ImageGallery;
