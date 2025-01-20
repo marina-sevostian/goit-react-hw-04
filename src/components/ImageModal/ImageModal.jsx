82130cbb783ab578873e818b6031aec257f0537b
Marina.S. <marina.sevostian@gmail.com>
2025-01-20 10:32:30 +0200

    fixed modal

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -12,30 +12,27 @@ const ImageModal = ({ isOpen, onCloseModal, image }) => {
       <button className={s.closeBtn} onClick={onCloseModal}></button>
       {image && (
         <div className={s.wrapImg}>
-          <div className={s.coverImg}>
-            <img
-              className={s.img}
-              src={image.urls.regular}
-              alt={image.alt_description}
-            />
-            <div className={s.imgInfo}>
-              <p className={s.descripImg}>
-                <span className={s.titleDescrip}>Description:</span>{' '}
-                {image.description}
-              </p>
-              <p className={s.descripImg}>
-                <span className={s.titleDescrip}>Image author:</span>{' '}
-                {image.user.name}
-              </p>
-              <p className={s.descripImg}>
-                <span className={s.titleDescrip}>Image author location:</span>{' '}
-                {image.user.location}
-              </p>
-              <p className={s.descripImg}>
-                <span className={s.titleDescrip}>Image likes:</span>{' '}
-                {image.likes}
-              </p>
-            </div>
+          <img
+            className={s.img}
+            src={image.urls.regular}
+            alt={image.alt_description}
+          />
+          <div className={s.imgInfo}>
+            <p className={s.descripImg}>
+              <span className={s.titleDescrip}>Description:</span>{' '}
+              {image.description}
+            </p>
+            <p className={s.descripImg}>
+              <span className={s.titleDescrip}>Image author:</span>{' '}
+              {image.user.name}
+            </p>
+            <p className={s.descripImg}>
+              <span className={s.titleDescrip}>Image author location:</span>{' '}
+              {image.user.location}
+            </p>
+            <p className={s.descripImg}>
+              <span className={s.titleDescrip}>Image likes:</span> {image.likes}
+            </p>
           </div>
         </div>
       )}
