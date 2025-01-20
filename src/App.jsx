988f15c42dd6a1c59cf7be5e375f87873b9d3e61
Marina.S. <marina.sevostian@gmail.com>
2025-01-20 04:55:46 +0200

    add loader error message and load more btn

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import { useEffect, useState } from 'react';
 import SearchBar from './components/SearchBar/SearchBar';
 import fetchImages from './services/api';
 import ImageGallery from './components/ImageGallery/ImageGallery';
+import ErrorMessage from './components/ErrorMessage/ErrorMessage';
+import Loader from './components/Loader/Loader';
 import { Toaster } from 'react-hot-toast';
+import LoaderMoreBtn from './components/LoadMoreBtn/LoadMoreBtn';
 import './App.css';
 
 function App() {
@@ -10,19 +13,23 @@ function App() {
   const [page, setPage] = useState(1);
   const [isError, setIsError] = useState(false);
   const [image, setImage] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
 
   useEffect(() => {
     if (!query) return;
     const getData = async () => {
       try {
         setIsError(false);
+        setIsLoading(true);
         const data = await fetchImages(query, page);
         setImage(prev => [...prev, ...data.results]);
+        setTotalPages(data.total_pages);
         console.log(data);
       } catch {
         setIsError(true);
       } finally {
-        console.log(getData);
+        setIsLoading(false);
       }
     };
     getData();
@@ -34,12 +41,20 @@ function App() {
     setPage(1);
   };
 
+  const handleChangePage = () => {
+    setPage(prev => prev + 1);
+  };
+
   return (
     <>
       <SearchBar setQuery={handleSetQuery} />
-      <Toaster />
-      {isError}
+      <Toaster position="top-right" />
+      {isError && <ErrorMessage />}
       <ImageGallery images={image} />
+      {isLoading && <Loader />}
+      {image.length > 0 && page < totalPages && (
+        <LoaderMoreBtn changePage={handleChangePage} />
+      )}
     </>
   );
 }
